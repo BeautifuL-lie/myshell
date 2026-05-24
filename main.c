edf8427c1ddd99bbb05a669437bf9f331318c353
Muhammad Ridwan Farizqi <mridwan6661@gmail.com>
2026-05-24 14:27:05 +0700

    refactor: seperate each functions

--- main.c
+++ main.c
@@ -8,172 +8,222 @@
 #define MAX_INPUT 256
 #define MAX_ARGS 64
 
+char *built_in_cmd[] = {"exit", "cd", "exec", NULL};
+
+void printprompt() {
+    putchar('\n');
+
+    char workdir[256];
+    getcwd(workdir, sizeof(workdir));
+
+    printf("%s\n", workdir);
+    printf("~> ");
+    fflush(stdout);
+}
+
+void getinput(char *buf) {
+    if (fgets(buf, MAX_INPUT, stdin) == NULL) {
+        exit(0);
+    }
+
+    buf[strcspn(buf, "\n")] = '\0';
+}
+
+int containPipe(char *input) {
+    int count = 0;
+    int index = 0;
+
+    while(input[index] != '\0') {
+        if (input[index] == '|') {
+            count++;
+        }
+        index++;
+    }
+
+    return count;
+}
+
+void parsePipe(char *input, char **args1, char **args2) {
+    char *pipe_pos = strchr(input, '|');
+    *pipe_pos = '\0';
+
+    char *end = pipe_pos - 1;
+    while(end > input && isspace((unsigned char)*end)) {
+        *end = '\0';
+        end--;
+    }
+
+    pipe_pos++;
+    while (*pipe_pos == ' ') pipe_pos++;
+
+    char *left = input;
+    char *right = pipe_pos;
+
+    char *ptr_l;
+    char *ptr_r;
+
+    int i = 0;
+
+    args1[i] = strtok_r(left, " ", &ptr_l);
+
+    while(args1[i] != NULL && i < MAX_ARGS - 1) {
+        i++;
+        args1[i] = strtok_r(NULL, " ", &ptr_l);
+    }
+    args1[i] = NULL;
+    
+    i = 0;
+
+    args2[i] = strtok_r(right, " ", &ptr_r);
+
+    while(args2[i] != NULL && i < MAX_ARGS - 1) {
+        i++;
+        args2[i] = strtok_r(NULL, " ", &ptr_r);
+    }
+    args2[i] = NULL;
+}
+
+void execpipe(char **args1, char **args2) {
+    int fd[2];
+    pipe(fd);
+
+    pid_t p1, p2;
+
+    p1 = fork();
+
+    if (p1 == 0) {
+        dup2(fd[1], STDOUT_FILENO);
+        close(fd[0]);
+        close(fd[1]);
+
+        execvp(args1[0], args1);
+        perror("myshell");
+        exit(1);
+    }
+
+    p2 = fork();
+
+    if (p2 == 0) {
+        dup2(fd[0], STDIN_FILENO);
+        close(fd[0]);
+        close(fd[1]);
+
+        execvp(args2[0], args2);
+        exit(1);
+    }
+
+    close(fd[0]);
+    close(fd[1]);
+
+    waitpid(p1, NULL, 0);
+    waitpid(p2, NULL, 0);
+}
+
+void parseInput(char *input, char **args) {
+    int i = 0;
+    char *saveptr;
+    args[i] = strtok_r(input, " \t\r\n", &saveptr);
+
+    while(args[i] != NULL && i < MAX_ARGS - 1) {
+        i++;
+        args[i] = strtok_r(NULL, " \t\r\n", &saveptr);
+    }
+
+    args[i] = NULL;
+}
+
+int isbuiltincmd(char *args) {
+    int i = 0;
+    
+    while (built_in_cmd[i] != NULL) {
+        if (strcmp(built_in_cmd[i], args) == 0) {
+            return 1;
+        }
+        i++;
+    }
+    return 0;
+}
+
+void execbuiltin(char **args) {
+    //EXIT
+    if (strcmp(args[0], "exit") == 0) exit(0);
+
+    //CD
+    if (strcmp(args[0], "cd") == 0) {
+        if (args[1] == NULL || strcmp(args[1], "~") == 0) {
+            if (chdir(getenv("HOME")) != 0) {
+                printf("cd: HOME environment not set\n");
+            }
+        } else {
+            if (chdir(args[1]) != 0) {
+                perror("cd");
+            }
+        }
+        return;
+    }
+
+    //EXEC
+    if (strcmp(args[0], "exec") == 0) {
+        if (args[1] == NULL) {
+            printf("exec needs argument!\n");
+            return;
+        }
+
+        execvp(args[1], args + 1);
+        perror("myshell");
+        return;
+    }   
+}
+
+void execcmd (char **args) {
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        execvp(args[0], args);
+        perror("myshell");
+        exit(1);
+    } else if (pid > 0) {
+        waitpid(pid, NULL, 0);
+    } else {
+        perror("myshell");
+    }
+}
+
 int main() {
     char input[MAX_INPUT];
     char *args[MAX_ARGS];
     char *args_r[MAX_ARGS];
 
     while (1) {
-        putchar('\n');
-
-        char workdir[256];
-        getcwd(workdir, sizeof(workdir));
-
-        printf("%s\n", workdir);
-        printf("~> ");
-        fflush(stdout);
-
-        if (fgets(input, sizeof(input), stdin) == NULL) {
-            break;
-        }
-
-        input[strcspn(input, "\n")] = '\0';
+        printprompt();
+        getinput(input);
 
         if (strlen(input) == 0) {
             continue;
         }
 
-        if (strcmp(input, "exit") == 0) {
-            break;
-        }
-
-        int i = 0;
-        char *pipePos = strchr(input, '|');
-        if (pipePos != NULL) {
-            int count = 0;
-            int index = 0;
-            while(input[index] != '\0') {
-                if (input[index] == '|') {
-                    count++;
-                }
-                index++;
-            }
-            if (count > 1) {
-                printf("myshell: only support 1 pipe rn -_-\n");
-                continue;
-            }
-            char *left;
-            char *right;
-
-            *pipePos = '\0';
-
-            char *end = pipePos - 1;
-            while(end > input && isspace((unsigned char)*end)) {
-                *end = '\0';
-                end--;
-            }
-
-            pipePos++;
-            while (*pipePos == ' ') pipePos++;
-
-            left = input;
-            right = pipePos;
-            
-            char *ptr_l;
-            char *ptr_r;
-
-            args[i] = strtok_r(left, " ", &ptr_l);
-
-            while(args[i] != NULL && i < MAX_ARGS - 1) {
-                i++;
-                args[i] = strtok_r(NULL, " ", &ptr_l);
-            }
-            args[i] = NULL;
-            
-            i = 0;
-
-            args_r[i] = strtok_r(right, " ", &ptr_r);
-
-            while (args_r[i] != NULL && i < MAX_ARGS - 1) {
-                i++;
-                args_r[i] = strtok_r(NULL, " ", &ptr_r);
-            }
-            args_r[i] = NULL;
-
-            int fd[2];
-            pipe(fd);
-
-            pid_t p1 = fork();
-
-            if (p1 == 0) {
-                dup2(fd[1], STDOUT_FILENO);
-                close(fd[0]);
-                close(fd[1]);
-
-                execvp(args[0], args);
-                perror("myshell");
-                exit(1);
-            }
-
-            pid_t p2 = fork();
-
-            if(p2 == 0) {
-                dup2(fd[0], STDIN_FILENO);
-                close(fd[1]);
-                close(fd[0]);
-
-                execvp(args_r[0], args_r);
-                perror("myshell");
-                exit(1);
-            }
-
-            close(fd[0]);
-            close(fd[1]);
-
-            waitpid(p2, NULL, 0);
-            waitpid(p1, NULL, 0);
-
+        int pipe_n = containPipe(input);
+        
+        if (pipe_n == 1) {
+            parsePipe(input, args, args_r);
+            execpipe(args, args_r);
             continue;
-        }
-
-        i = 0;
-        char *saveptr;
-        args[i] = strtok_r(input, " ", &saveptr);
-
-        while(args[i] != NULL && i < MAX_ARGS - 1) {
-            i++;
-            args[i] = strtok_r(NULL, " ", &saveptr);
-        }
-
-        args[i] = NULL;
-
-        // Built-in CD
-        if (strcmp(args[0], "cd") == 0) {
-            if (args[1] == NULL || strcmp(args[1], "~") == 0) {
-                if (chdir(getenv("HOME")) != 0) {
-                    printf("cd: HOME environment not set\n");
-                }
-            } else {
-                if (chdir(args[1]) != 0) {
-                    perror("cd");
-                }
-            }
+        } else if (pipe_n > 1) {
+            printf("myshell: only support 1 pipe rn -_-\n");
             continue;
         }
         
-        //Built-in exec
-        if (strcmp(args[0], "exec") == 0) {
-            if (args[1] == NULL) {
-                printf("exec needs argument!\n");
-                continue;
-            }
-
-            execvp(args[1], args + 1);
-            perror("myshell");
+        parseInput(input, args);
+        if (args[0] == NULL) {
             continue;
         }
 
-        pid_t pid = fork();
-
-        if (pid == 0) {
-            execvp(args[0], args);
-            perror("myshell");
-            exit(1);
-        } else if (pid > 0) {
-            waitpid(pid, NULL, 0);
-        } else {
-            perror("myshell");
+        if (isbuiltincmd(args[0])) {
+            execbuiltin(args);
+            continue;
         }
+
+        execcmd(args);
     }
 
     return 0;
