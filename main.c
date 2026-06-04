1325164042622c5e8ea4b39028eebb448445ee65
Muhammad Ridwan Farizqi <mridwan6661@gmail.com>
2026-06-04 20:25:12 +0700

    refactor(printprompt): delete gethomedir, use getenv instead

--- main.c
+++ main.c
@@ -11,31 +11,24 @@
 
 char *built_in_cmd[] = {"exit", "cd", "exec", NULL};
 
-void gethomedir(char *buf, size_t size) {
-   struct passwd *pw = getpwuid(getuid());
-
-    if (pw) {
-        snprintf(buf, size, "/home/%s", pw->pw_name);
-    }
-}
-
 void printprompt() {
     putchar('\n');
 
     char workdir[256];
     getcwd(workdir, sizeof(workdir));
-    
-    char homedir[256];
-    gethomedir(homedir, sizeof(homedir));
 
-    char *homeptr = strstr(workdir, homedir);
+    char *homedir = getenv("HOME");
 
-    if (homeptr != NULL) {
-        printf("~%s\n", homeptr + strlen(homedir));
-        printf("~> ");
-        fflush(stdout);
+    if (homedir != NULL) {
+        char *homeptr = strstr(workdir, homedir);
 
-        return;
+        if (homeptr != NULL) {
+            printf("~%s\n", homeptr + strlen(homedir));
+            printf("~> ");
+            fflush(stdout);
+
+            return;
+        }
     }
 
     printf("%s\n", workdir);
